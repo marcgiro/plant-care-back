24fa3c05f1cffa050616ba91e7a38798680d9f79
Marc Giro <marc.piraces@cognativinc.com>
2022-07-04 17:51:08 +0200

    created plant entity and connected it to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Plant } from './plants/plant.entity';
 import { PlantsModule } from './plants/plants.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
@@ -11,7 +12,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Plant],
       synchronize: true,
     }),
     UsersModule,
---
+++ src/plants/plant.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Plant {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  variety: string;
+
+  @Column()
+  health: number;
+
+  @Column()
+  isAlive: boolean;
+
+  @Column()
+  lastWatered: Date;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
