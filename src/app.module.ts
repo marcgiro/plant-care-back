6a967b110c50c7dc99abeb671188e5cb69c2684f
Marc Giro <marc.piraces@cognativinc.com>
2022-07-04 17:34:08 +0200

    database connection set up and user entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlantsModule } from './plants/plants.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, PlantsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    PlantsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
