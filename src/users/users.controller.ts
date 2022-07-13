0d2d44dd3cb76051dc92529d034062e2e3be83bd
Marc Giro <marc.piraces@cognativinc.com>
2022-07-13 16:29:05 +0200

    added basic user methods findOne, find, delete and update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
+import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UsersService } from "./users.service";
 
 @Controller("auth")
@@ -9,5 +18,25 @@ export class UsersController {
   @Post("/signup")
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.name, body.email, body.password);
+  }
+
+  @Get("/:id")
+  findOneUser(@Param("id") id: string) {
+    return this.usersService.findOneById(parseInt(id));
+  }
+
+  @Get("/")
+  findAllUsers() {
+    return this.usersService.find();
+  }
+
+  @Delete("/:id")
+  deleteUser(@Param("id") id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
