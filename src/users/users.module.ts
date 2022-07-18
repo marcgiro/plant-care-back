0c09ce808a89a8c06e62d7e2ce3a19ab6faece1b
Marc Giro <marc.piraces@cognativinc.com>
2022-07-18 14:30:32 +0200

    added the CurrentUserInterceptor to the users module provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,19 @@ import { User } from "./user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { AuthService } from "./auth/auth.service";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { CurrentUserInterceptor } from "./interceptors/current-user.interceptor";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
